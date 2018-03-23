678df5bf5e056e6d5f55db22cf11fa9791c4bca5
kawboyer <kawboyer@gmail.com>
2018-03-23 10:38:13 -0400

    UserSearch function added to newUserSearch

---
+++ newUserSearch.js
@@ -0,0 +1,23 @@
+var weather = require("weather-js");
+
+var UserSearch = function (name, location) {
+  this.name = name;
+  this.location = location;
+  thids.date = Date.now(); // Look at the documentation for the date object. Or use Moment. Moment requires an npm!
+
+  this.getWeather = function () {
+
+    weather.find({ search: this.location, degreeType: "F" }, function (err, result) {
+
+      if (err) return console.log(err);
+
+      console.log(JSON.stringify(result, null, 2));
+    });
+  };
+};
+
+var testSearch = new UserSearch("name", "location");
+console.log(testSearch);
+// testSearch.getWeather(); - This is going to happen in one of our other modules.
+
+module.exports = UserSearch;
